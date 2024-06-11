373163d79d7d379ca0433eeaa7425d23608e5029
spacedogstudios <darren@spacedog.com.au>
2024-06-11 11:40:33 +1000

    build: add husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [
+    buildPrettierCommand,
+    buildEslintCommand,
+    "jest --bail --findRelatedTests --passWithNoTests",
+  ],
+};
